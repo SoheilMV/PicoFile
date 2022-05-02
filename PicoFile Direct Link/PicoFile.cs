636ffeb4d0f566eda5cb509e73b4802f41780abd
SoheilMV <47953001+SoheilMV@users.noreply.github.com>
2022-05-02 22:07:35 +0430

    Update PicoFile.cs

--- PicoFile Direct Link/PicoFile.cs
+++ PicoFile Direct Link/PicoFile.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +7,9 @@ namespace PicoFile_Direct_Link
 {
     public class PicoFile
     {
-        string url = string.Empty;
-        string host = string.Empty;
-        string num = string.Empty;
-        string address = string.Empty;
+        private string _url = string.Empty;
+        private string _host = string.Empty;
+        private string _num = string.Empty;
 
         public PicoFile()
         {
@@ -22,46 +19,51 @@ namespace PicoFile_Direct_Link
         {
             get
             {
-                return url;
+                return _url;
             }
             set
             {
-                url = value;
-                string[] array = url.Split(new char[] { '/' });
-                host = array[2];
-                num = array[4];
-                address = "http://" + host + "/file/GenerateDownloadLink?fileId=" + num;
+                _url = value;
+                string[] array = _url.Split(new char[] { '/' });
+                _host = array[2];
+                _num = array[4];
             }
         }
 
         public async Task<string> DirectLink()
         {
-            CookieContainer cc = new CookieContainer();
             using (HttpClientHandler hch = new HttpClientHandler())
             {
                 hch.AllowAutoRedirect = false;
-                hch.CookieContainer = cc;
-                hch.UseProxy = false;
-                using (HttpClient hc = new HttpClient(hch))
+                using (HttpClient hc = new HttpClient(hch, true))
                 {
-                    hc.DefaultRequestHeaders.Add("Referer", url);
-                    hc.DefaultRequestHeaders.Add("X-Requested-With", "XMLHttpRequest");
-                    var res = await hc.PostAsync(address, new StringContent(""));
-                    if (res.IsSuccessStatusCode)
+                    using (HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, $"https://{_host}/file/GenerateDownloadLink?fileId={_num}"))
                     {
-                        return await res.Content.ReadAsStringAsync();
-                    }
-                    else if (res.StatusCode == HttpStatusCode.Forbidden)
-                    {
-                        throw new PicoFileException("Invalid password!");
-                    }
-                    else if (res.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        throw new PicoFileException("Unable to download this file at this time!");
-                    }
-                    else
-                    {
-                        throw new PicoFileException("Error performing operation!");
+                        req.Content = new StringContent("", Encoding.UTF8, "application/x-www-form-urlencoded");
+                        req.Headers.Add("Accept", "*/*");
+                        req.Headers.Add("Origin", "https://s8.picofile.com");
+                        req.Headers.Add("Referer", _url);
+                        req.Headers.Add("X-Requested-With", "XMLHttpRequest");
+                        req.Headers.ExpectContinue = false;
+                        using (HttpResponseMessage res = await hc.SendAsync(req))
+                        {
+                            if (res.IsSuccessStatusCode)
+                            {
+                                return await res.Content.ReadAsStringAsync();
+                            }
+                            else if (res.StatusCode == HttpStatusCode.Forbidden)
+                            {
+                                throw new PicoFileException("Invalid password!");
+                            }
+                            else if (res.StatusCode == HttpStatusCode.NotFound)
+                            {
+                                throw new PicoFileException("Unable to download this file at this time!");
+                            }
+                            else
+                            {
+                                throw new PicoFileException("Error performing operation!");
+                            }
+                        }
                     }
                 }
             }
@@ -69,32 +71,38 @@ namespace PicoFile_Direct_Link
 
         public async Task<string> DirectLink(string password)
         {
-            CookieContainer cc = new CookieContainer();
             using (HttpClientHandler hch = new HttpClientHandler())
             {
                 hch.AllowAutoRedirect = false;
-                hch.CookieContainer = cc;
-                hch.UseProxy = false;
-                using (HttpClient hc = new HttpClient(hch))
+                using (HttpClient hc = new HttpClient(hch, true))
                 {
-                    hc.DefaultRequestHeaders.Add("Referer", url);
-                    hc.DefaultRequestHeaders.Add("X-Requested-With", "XMLHttpRequest");
-                    var res = await hc.PostAsync(address, new StringContent("password=" + password, Encoding.UTF8, "application/x-www-form-urlencoded"));
-                    if (res.IsSuccessStatusCode)
+                    using (HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, $"https://{_host}/file/generateDownloadLink?fileId={_num}"))
                     {
-                        return await res.Content.ReadAsStringAsync();
-                    }
-                    else if (res.StatusCode == HttpStatusCode.Forbidden)
-                    {
-                        throw new PicoFileException("Invalid password!");
-                    }
-                    else if (res.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        throw new PicoFileException("Unable to download this file at this time!");
-                    }
-                    else
-                    {
-                        throw new PicoFileException("Error performing operation!");
+                        req.Content = new StringContent("password=" + password, Encoding.UTF8, "application/x-www-form-urlencoded");
+                        req.Headers.Add("Accept", "*/*");
+                        req.Headers.Add("Origin", "https://s8.picofile.com");
+                        req.Headers.Add("Referer", _url);
+                        req.Headers.Add("X-Requested-With", "XMLHttpRequest");
+                        req.Headers.ExpectContinue = false;
+                        using (HttpResponseMessage res = await hc.SendAsync(req))
+                        {
+                            if (res.IsSuccessStatusCode)
+                            {
+                                return await res.Content.ReadAsStringAsync();
+                            }
+                            else if (res.StatusCode == HttpStatusCode.Forbidden)
+                            {
+                                throw new PicoFileException("Invalid password!");
+                            }
+                            else if (res.StatusCode == HttpStatusCode.NotFound)
+                            {
+                                throw new PicoFileException("Unable to download this file at this time!");
+                            }
+                            else
+                            {
+                                throw new PicoFileException("Error performing operation!");
+                            }
+                        }
                     }
                 }
             }
